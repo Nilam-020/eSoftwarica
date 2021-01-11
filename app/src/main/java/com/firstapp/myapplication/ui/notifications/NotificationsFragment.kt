a9c5ae664f1f192374080baa87e8e4abad911e9a
ankit016 <alyohinadhikari@gmail.com>
2021-01-11 10:36:07 +0545

    new file uploaded

---
+++ app/src/main/java/com/firstapp/myapplication/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,43 @@
+package com.firstapp.myapplication.ui.notifications
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.firstapp.myapplication.R
+
+class NotificationsFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var webView: WebView
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+//        notificationsViewModel =
+//            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val view = inflater.inflate(R.layout.fragment_notifications, container, false)
+        webView = view!!.findViewById(R.id.webView)
+        webView.loadUrl("https:www.softwarica.edu.np")
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled=true
+
+
+//        val textView: TextView = root.findViewById(R.id.text_notifications)
+//        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        return view
+    }
+}
